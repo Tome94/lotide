63c19a974563336d59cddc797e9a756aedff031e
Tome94 <tommy_yang621@hotmail.com>
2022-02-25 22:31:05 +0000

    own console.assert

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+let emojiMad = String.fromCodePoint(0x1F621);
+let emojiHappy = String.fromCodePoint(0x1F603);
+
+const assertEqual = function(actual, expected) {
+  if (actual  === expected) {
+    console.log(emojiHappy + "Assertion passed: " + actual + " === " + expected);
+  } else {
+    console.log(emojiMad + "Assertion failed: " + actual + " !== " + expected);
+  }
+};
+
+//test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
