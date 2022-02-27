f880da8f566aef03a6b6afb31bfd953f430ed123
Tome94 <tommy_yang621@hotmail.com>
2022-02-27 03:27:49 +0000

    fix so it better for it to be always true unless its false

--- eqArrays.js
+++ eqArrays.js
@@ -10,13 +10,11 @@ const assertEqual = function(actual, expected) {
 };
 const eqArrays = function (array1, array2){
   for (let i = 0; i < array1.length; i++){
-    if (array1[i]===array2[i]){
-      return true
-    }
-    else {
+    if (array1[i]!==array2[i]){
       return false
     }
   }
+  return true
 }
 
 //test code
